0406921c767b99f095bbf2e348f81ab2dfd0d980
Caian Ertl <hi@caian.org>
2024-06-01 22:38:30 -0300

    feat: use the chromium binary provided at `CHROMIUM_BROWSER_BINARY_PATH` if set

--- internal/widget_browser/exec_local.go
+++ internal/widget_browser/exec_local.go
@@ -3,12 +3,21 @@
 package browser
 
 import (
+	"os"
+
 	// 3rd-party
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
 )
 
 func getBrowser() (*launcher.Launcher, *rod.Browser) {
-	u := launcher.New()
+	var u *launcher.Launcher
+
+	if binp, binpIsSet := os.LookupEnv("CHROMIUM_BROWSER_BINARY_PATH"); binpIsSet {
+		u = launcher.New().Bin(binp)
+	} else {
+		u = launcher.New()
+	}
+
 	return u, rod.New().ControlURL(u.MustLaunch())
 }
